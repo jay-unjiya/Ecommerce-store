130d2ef2a253ba6017cfee296b581bafe24cc4bb
Jay Unjiya <jay.u@huptechweb.com>
2025-02-19 17:04:56 +0530

    send user detail from backend

--- backend/routes/authCheck.js
+++ backend/routes/authCheck.js
@@ -3,7 +3,7 @@ const authCheck = require('../middleware/adminAuth');
 const route = express.Router();
 
 route.post('/verifyAccess', authCheck, (req, res) => {
-    res.status(200).json({ message: 'Access Granted', success: true, id: req.user.id });
+    res.status(200).json({ message: 'Access Granted', success: true, id: req.user.id ,user:req.user});
 });
 
 route.post('/verifyAdminAccess', authCheck, (req, res) => {
